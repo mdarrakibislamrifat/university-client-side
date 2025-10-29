941d0c38970b5bc0a3dd64281094478a5df31ca3
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-10-29 12:44:55 +0600

    Fixing the layout and setting up basic sidebar options

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import MainLayout from "./components/layout/MainLayout"
 
 function App() {
 
   return (
-    <div>
-      Home Page
-    </div>
+    <MainLayout />
   )
 }
 
---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,9 @@
+
+
+const AdminLayout = () => {
+    return (
+        <div>This is AdminLayout</div>
+    )
+}
+
+export default AdminLayout
